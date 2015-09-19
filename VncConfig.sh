138a5ca67842b3e4cec898fee50c7a40c4f475ce
SanUSB <sanusb_laese@yahoo.com.br>
2015-09-19 13:38:57 -0300

    v1
    v1

---
+++ VncConfig.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Install as pi user (exit  - without sudo su)
+
+    sudo apt-get update
+	sudo apt-get install tightvncserver
+    sudo wget http://sanusb.org/tools/VncConfigUserPi.deb
+    sudo dpkg -i VncConfigUserPi.deb
+	sudo reboot
+
+
+
